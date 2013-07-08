8cf8b0645868fe86dab8b100575616bf62d42a21
JohnnyPP <johnnnyppp@gmail.com>
2013-07-09 00:26:31 +0200

    Label textblock updated automatically with textbox
    Second button updates the label with the data taken from second text box

--- WpfApplicationPropertyChanged/ViewModel.cs
+++ WpfApplicationPropertyChanged/ViewModel.cs
@@ -13,6 +13,8 @@ namespace WpfApplicationPropertyChanged
         public ViewModel()
         {
             updateControl="Press the button";
+            updateLabel = "Label updated";
+            updateTextBox = "TestTextBox";
         }
 
         int count = 0;
@@ -34,6 +36,38 @@ namespace WpfApplicationPropertyChanged
             }
         }
 
+        private string updateLabel;
+
+        public string UpdateLabel
+        {
+            get { return updateLabel; }
+            set
+            {
+                if (updateLabel != value)
+                {
+                    updateLabel = value;
+                    RaisePropertyChanged("UpdateLabel");
+                }
+                updateLabel = value;
+            }
+        }
+
+        private string updateTextBox;
+
+        public string UpdateTextBox
+        {
+            get { return updateTextBox; }
+            set
+            {
+                if (updateTextBox != value)
+                {
+                    updateTextBox = value;
+                    RaisePropertyChanged("UpdateTextBox");      //Updates the right-bottom TextBox
+                }                                               //Text="{Binding Path=UpdateTextBox, Mode=TwoWay}" 
+                updateTextBox = value;                          //The content of the textbox is transferred to the
+            }                                                   //UpdateTextBox property and may be further used 
+        }
+
         private void RaisePropertyChanged(string propertyName)
         {
             // take a copy to prevent thread issues
@@ -47,19 +81,49 @@ namespace WpfApplicationPropertyChanged
 
         public event PropertyChangedEventHandler PropertyChanged;           //implemented automatically
 
+        //All the buttons require 2 functions that names are defined in
+        //get { return new RelayCommand(1. Execute, 2. CanNAMEExecute)
+      
+
         #region Commands
-        void UpdateArtistNameExecute()
+        void UpdateControlExecute()
         {
             ++count;
             UpdateControl = string.Format("You pressed the button ({0}) many times", count);
         }
 
-        bool CanUpdateArtistNameExecute()
+        bool CanUpdateControlExecute()
         {
             return true;
         }
 
-        public ICommand UpdateArtistName { get { return new RelayCommand(UpdateArtistNameExecute, CanUpdateArtistNameExecute); } }
-        #endregion
+        public ICommand UpdateName 
+        { 
+            get { return new RelayCommand(UpdateControlExecute, CanUpdateControlExecute); } 
+        }
+
+        ////////////////////////////////////////
+        //UpdateLabel
+        ///////////////////////////////////////
+
+        void UpdateLabelControlExecute()
+        {
+            //++count;
+            UpdateLabel = string.Format("({0})+({1})", count, updateTextBox);
+        }
+
+        bool CanUpdateLabelControlExecute()
+        {
+            return true;
+        }
+
+        public ICommand UpdateLabelName //ICommand name must be the same as the Button binding name in XAML 
+        {                               //<Button Content="Button" Command="{Binding UpdateLabelName}"/>
+            get { return new RelayCommand(UpdateLabelControlExecute, CanUpdateLabelControlExecute); }
+        }                               //The UpdateLabelControlExecute and CanUpdateLabelControlExecute must be defined
+                                        //In the UpdateLabelControlExecute() function is the body that will be executed
+                                        //After pressing the button
+        #endregion                      
+
     }
 }
